a24f51604688bf7590a6795d51c22f64f4c2a716
Antkeo1 <sleav143@gmail.com>
2019-05-17 16:43:48 -0400

    starting google oauth code

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express')
+const passport = require('passport')
+const GoogleStrategy = require('passport-google-oauth20').Strategy
 
 const app = express()
 
-//route handler
-app.get('/', (req, res) => {
-  res.send({saiyan: 'Broly is strongest'})
-})
+passport.use(new GoogleStrategy())
+
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT)

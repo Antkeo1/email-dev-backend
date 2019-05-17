1c100b3e4baa4d4cd540945819d7e3305248fe50
Antkeo1 <sleav143@gmail.com>
2019-05-17 14:28:51 -0400

    testing

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express()
 
 //route handler
 app.get('/', (req, res) => {
-  res.send({hi: 'there'})
+  res.send({saiyan: 'Broly is strongest'})
 })
 
 const PORT = process.env.PORT || 5000

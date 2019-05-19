50912d9ab2085e857218487675062d8706cd5655
Antkeo1 <sleav143@gmail.com>
2019-05-19 14:27:37 -0400

    refactored gitignore file

---
+++ config/prod.js
@@ -0,0 +1,7 @@
+// mongoProdURI = 'mongodb+srv://Antkeo:Antkeo1990@cluster0-udkvc.mongodb.net/test?retryWrites=true'
+module.exports = {
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY
+}

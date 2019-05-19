d400efa6eca965a6c2622a09e672a7c0b1475e0b
Antkeo1 <sleav143@gmail.com>
2019-05-19 14:38:32 -0400

    finished auth

--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,4 @@
-// mongoProdURI = 'mongodb+srv://Antkeo:Antkeo1990@cluster0-udkvc.mongodb.net/test?retryWrites=true'
+// mongoProdURI = 'mongodb+srv://Antkeo:Antkeo1990!@cluster0-udkvc.mongodb.net/test?retryWrites=true'
 module.exports = {
   googleClientID: process.env.GOOGLE_CLIENT_ID,
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,

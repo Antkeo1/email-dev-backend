ee85bbd7044d1889f13e42933f77cf7a71d5702b
Antkeo1 <sleav143@gmail.com>
2019-05-19 20:37:01 -0400

    added proxy:true to passport.js

--- services/passport.js
+++ services/passport.js
@@ -22,7 +22,8 @@ passport.use(
     {
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
-      callbackURL: '/auth/google/callback'
+      callbackURL: '/auth/google/callback',
+      proxy: true
     },
     (accessToken, refreshToken, profile, done) => {
       // to find accounts where googleId matches profile id

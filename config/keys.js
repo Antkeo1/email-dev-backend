93e433f376a364f671f16b6b99a3c79e25157740
Antkeo1 <sleav143@gmail.com>
2019-05-19 22:02:06 -0400

    tweaked User.js

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,6 @@
 if (process.env.NODE_ENV === 'production') {
   // we are in production - return prod set of keys
-  <a href="https://module.exports" rel="nofollow" target="_blank">module.exports</a> = require('./prod');
-}
+  module.exports = require('./prod')
 } else {
   // we are in development - return the dev keys
   module.exports = require('./dev')

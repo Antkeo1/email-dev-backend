b98012a6dfe8868624dbfd99b4c242a60d9b4aff
Antkeo1 <sleav143@gmail.com>
2019-05-22 18:46:22 -0400

    added proxy

--- client/src/App.js
+++ client/src/App.js
@@ -5,14 +5,9 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>App!!</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="container">
+        App
+        <a href='/auth/google'>Sign In With Google</a>
       </div>
     );
   }

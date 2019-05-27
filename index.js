0dc313723fbe4ee712403d8f6ce6e142bdd8a76d
Antkeo1 <sleav143@gmail.com>
2019-05-27 10:00:20 -0400

    now able to use to stripe to purchase credits

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const cookieSession = require('cookie-session')
 const passport = require('passport')
+const bodyParser = require('body-parser')
 const keys = require('./config/keys')
 require('./models/user')
 require('./services/passport')
@@ -10,6 +11,7 @@ mongoose.connect(keys.mongoURI)
 
 const app = express()
 
+app.use(bodyParser.json())
 app.use(
   cookieSession({
     maxAge: 30 * 24 * 60 * 60 * 1000,
@@ -19,7 +21,10 @@ app.use(
 app.use(passport.initialize())
 app.use(passport.session())
 
+
+//routes
 require('./routes/authRoutes')(app)
+require('./routes/billingRoutes')(app)
 
 app.listen(process.env.PORT || 5000, function(){
     console.log('Your node js server is running');

068ec0c765b26f92308a3d03177e183486cdbcb8
Antkeo1 <sleav143@gmail.com>
2019-06-14 13:42:53 -0400

    now able to send out emails

---
+++ client/src/components/surveys/SurveyNew.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { reduxForm } from 'redux-form'
+import SurveyForm from './SurveyForm'
+import SurveyReview from './SurveyFormReview'
+
+class SurveyNew extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      showFormReview: false
+    }
+  }
+
+  renderContent() {
+    if (this.state.showFormReview) {
+      return <SurveyReview
+        onCancel={() => this.setState({
+          showFormReview: false
+        })}
+      />
+    }
+    return <SurveyForm onSurveySubmit={() => this.setState({showFormReview: true})}/>
+  }
+
+  render() {
+    return (
+      <div>
+        {this.renderContent()}
+      </div>
+    )
+  }
+}
+
+export default reduxForm({
+  form: 'surveyForm'
+})(SurveyNew)
